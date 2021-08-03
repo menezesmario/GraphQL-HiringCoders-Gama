7573fb7681fad03cf2c4d3b6f8997f5c90ffda4b
menezesmario <mario082@gmail.com>
2021-08-03 15:57:46 -0300

    redirecionamento de autenticação

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createServer } from 'http';
+import { readFile } from 'fs';
+import { resolve } from 'path';
+import { parse } from 'querystring';
 
 const server = createServer((request, response) => {
     switch(request.url){
@@ -6,11 +9,57 @@ const server = createServer((request, response) => {
             response.writeHead(200, {
                 'Content-Type': 'application/json',
             });
-            response.write(JSON.stringify({
-                status: 'Okay',
-            }));
+            response.write(
+                JSON.stringify({
+                    status: 'Okay',
+                })
+            );
             response.end();
-            return;
+            break;
+        }
+        case '/home': {
+            const path = resolve(__dirname, './pages/home.html');
+            readFile(path, (error, file) => {
+                if (error) {
+                    response.writeHead(500, 'Can\'t process HTML file.');
+                    response.end();
+                    return;
+                }
+
+                response.writeHead(200);
+                response.write(file);
+                response.end();
+            })
+            break;
+        }
+        case '/sign-in': {
+            const path = resolve(__dirname, './pages/sign-in.html');
+            readFile(path, (error, file) => {
+                if (error) {
+                    response.writeHead(500, 'Can\'t process HTML file.');
+                    response.end();
+                    return;
+                }
+
+                response.writeHead(200);
+                response.write(file);
+                response.end();
+            })
+            break;
+        }
+        case '/authenticate': {
+            let data = '';
+            request.on('data', (chunk) => {
+                data += chunk;
+            });
+            request.on('end', () => {
+                const params = parse(data);
+                response.writeHead(301, {
+                    Location: '/home',
+                });
+                response.end();
+            })
+            break;
         }
         default: {
             response.writeHead(404, 'Service not found.');
